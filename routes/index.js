82631865f0d9ae6d270d28b2d4e1a14c40099496
DakotahMW <156035461+DakotahMW@users.noreply.github.com>
2025-12-08 05:39:08 +0000

    completed

--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,89 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next){
-  try {
-    req.db.query('SELECT * FROM todos;', (err, results) => {
-      if (err) {
-        console.error('Error fetching todos:', err);
-        return res.status(500).send('Error fetching todos');
-      }
-      res.render('index', { title: 'My Simple TODO', todos: results, mode: 'light' });
+/* additional step for starting database (before npm start)
+  use -> sudo mysql -u root -p < ./setup_scripts/create_comments_table.sql
+*/
+
+
+/* function for returning middleware function with either light or dark mode*/
+function homeMode(mode){
+  return function(req, res, next){
+    res.render('index', {mode: mode});
+  }
+};
+
+function aboutUsMode(mode){
+  return function(req, res, next){
+    res.render('aboutUs', {mode: mode});
+  }
+}
+
+function commentMode(mode){
+  /* get comment page and it list. */
+  return function(req, res, next){
+    
+    try {
+      req.db.query('SELECT * FROM comments;', (err, results) => {
+        if (err) {
+          console.error('Error fetching comments list:', err);
+          return res.status(500).send('Error fetching comments');
+        }
+        res.render('comments', {mode: mode, comments: results});
     });
   } catch (error) {
-    console.error('Error fetching items:', error);
-    res.status(500).send('Error fetching items');
+    console.error('Error fetching comments:', error);
+    res.status(500).send('Error fetching comments');
   }
-});
+  }
+}
 
-router.post('/create', function (req, res, next) {
-    const { task } = req.body;
+function menuMode(mode){
+  return function(req, res, next){
+    res.render('menu', {mode: mode});
+  }
+}
+
+
+/* manage home page */
+ router.get('/', function(req, res, next){
+    res.redirect('/light'); 
+ });
+
+ router.get('/light', homeMode('light'));
+ router.get('/dark', homeMode('dark'));
+
+ // manage about us page
+
+ router.get('/about_us/light', aboutUsMode('light'));
+ router.get('/about_us/dark', aboutUsMode('dark'));
+
+
+ /* manage comment page */
+ router.get('/comments/light', commentMode('light'));
+ router.get('/comments/dark', commentMode('dark'));
+ 
+    //posting the comment
+ router.post('/comments/post', function (req, res, next) {
+    const { mode, comment } = req.body;
     try {
-      req.db.query('INSERT INTO todos (task) VALUES (?);', [task], (err, results) => {
+      req.db.query('INSERT INTO comments (detail) VALUES (?);', [comment], (err, results) => {
         if (err) {
-          console.error('Error adding todo:', err);
-          return res.status(500).send('Error adding todo');
+          console.error('Error posting comment:', err);
+          return res.status(500).send('Error posting comment');
         }
-        console.log('Todo added successfully:', results);
-        // Redirect to the home page after adding
-        res.redirect('/');
+        console.log('Comment added successfully:', results);
+        // Redirect to comment page
+        res.redirect('/comments/' + mode);
       });
     } catch (error) {
-      console.error('Error adding todo:', error);
-      res.status(500).send('Error adding todo');
+      console.error('Error adding comment:', error);
+      res.status(500).send('Error adding comment');
     }
 });
 
-router.post('/delete', function (req, res, next) {
-    const { id } = req.body;
-    try {
-      req.db.query('DELETE FROM todos WHERE id = ?;', [id], (err, results) => {
-        if (err) {
-          console.error('Error deleting todo:', err);
-          return res.status(500).send('Error deleting todo');
-        }
-        console.log('Todo deleted successfully:', results);
-        // Redirect to the home page after deletion
-        res.redirect('/');
-    });
-    }catch (error) {
-        console.error('Error deleting todo:', error);
-        res.status(500).send('Error deleting todo:');
-    }
-});
+ /* manage menu page */
+ router.get('/menu/light', menuMode('light'));
+ router.get('/menu/dark', menuMode('dark'));
 
 module.exports = router;
